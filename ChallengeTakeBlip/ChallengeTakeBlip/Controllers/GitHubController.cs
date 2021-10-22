58b47277c17836b7a1de8c783e0f364c89be04bd
jackeline.moura <jackeline.moura@indigosoft.com.br>
2021-10-22 16:29:40 -0300

    initial structure of the controller and model

---
+++ ChallengeTakeBlip/ChallengeTakeBlip/Controllers/GitHubController.cs
@@ -0,0 +1,48 @@
+﻿using ChallengeTakeBlip.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ChallengeTakeBlip.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GitHubController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetRepositoryGitHub()
+        {
+            string ApiBaseUrl = "https://api.github.com/orgs/takenet/repos";
+
+            var model = new GitHubModel();
+            try
+            {
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(ApiBaseUrl);
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Method = "GET";
+                httpWebRequest.UserAgent = "request";
+
+                using (HttpWebResponse response = (HttpWebResponse)httpWebRequest.GetResponse())
+                using (Stream stream = response.GetResponseStream())
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string responseString = reader.ReadToEnd();
+                }
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+
+            return Ok();
+        }
+    }
+}
---
+++ ChallengeTakeBlip/ChallengeTakeBlip/Model/GitHubModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+
+namespace ChallengeTakeBlip.Model
+{
+    public class GitHubModel
+    {
+        [JsonPropertyName("avatar_url")]
+        public string AvatarUrl { get; set; }
+
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+
+        [JsonPropertyName("full_name")]
+        public string FullName { get; set; }
+
+
+        [JsonPropertyName("owner")]
+        public string Owner { get; set; }
+
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+
+        [JsonPropertyName("created_at")]
+        public DateTime CreatedAt { get; set; }
+
+
+        [JsonPropertyName("updated_at")]
+        public string UpdateAt { get; set; }
+
+
+        [JsonPropertyName("language")]
+        public string Language { get; set; }
+
+
+        [JsonPropertyName("languages_url")]
+        public string LanguagesUrl { get; set; }
+
+        [JsonPropertyName("html_url")]
+        public string LinkRepo { get; set; }
+    }
+}
