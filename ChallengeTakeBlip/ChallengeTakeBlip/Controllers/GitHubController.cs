b3a0a50b11b0ca7267db4465929e553750c79df8
jackeline.moura <jackeline.moura@indigosoft.com.br>
2021-10-24 00:08:22 -0300

    Returning json to a list at Controller and update Model

--- ChallengeTakeBlip/ChallengeTakeBlip/Controllers/GitHubController.cs
+++ ChallengeTakeBlip/ChallengeTakeBlip/Controllers/GitHubController.cs
@@ -1,5 +1,6 @@
 ﻿using ChallengeTakeBlip.Model;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -14,13 +15,14 @@ namespace ChallengeTakeBlip.Controllers
     [Route("api/[controller]")]
     public class GitHubController : ControllerBase
     {
+        public List<GitHubModel> gitHubData { get; set; } = new List<GitHubModel>();
+        public List<BotData> botData { get; set; } = new List<BotData>();
 
         [HttpGet]
-        public IActionResult GetRepositoryGitHub()
+        public List<BotData> GetRepositoryGitHub()
         {
             string ApiBaseUrl = "https://api.github.com/orgs/takenet/repos";
 
-            var model = new GitHubModel();
             try
             {
                 var httpWebRequest = (HttpWebRequest)WebRequest.Create(ApiBaseUrl);
@@ -28,21 +30,34 @@ namespace ChallengeTakeBlip.Controllers
                 httpWebRequest.Method = "GET";
                 httpWebRequest.UserAgent = "request";
 
+                var responseString = "";
+
                 using (HttpWebResponse response = (HttpWebResponse)httpWebRequest.GetResponse())
                 using (Stream stream = response.GetResponseStream())
                 using (StreamReader reader = new StreamReader(stream))
                 {
-                    string responseString = reader.ReadToEnd();
+                    responseString = reader.ReadToEnd();
                 }
 
+                var jsonConvert = JsonConvert.DeserializeObject<List<GitHubModel>>(responseString);
+                var json = jsonConvert.Take(5).ToList();
+
+                var list = new BotData();
+
+                foreach (var item in json)
+                {
+                    string nameRepository = item.full_name;
+                    string description = item.description;
+                    string urlAvatar = item.owner.avatar_url;
+
+                    botData.Add(new BotData{RepositoryName = nameRepository, RepositoryDescription = description, Avatar = urlAvatar});
+                }
             }
             catch (Exception e)
             {
                 throw e;
             }
-
-
-            return Ok();
+            return botData;
         }
     }
 }
